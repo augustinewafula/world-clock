2c0c4ebc2e75350fcee991e55dbc964bd00090ef
augustinewafula <augustinetreezy@outlook.com>
2020-08-13 20:27:56 +0300

    create routes. create button on home screen to navigate to location screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:world_clock/pages/choose_location.dart';
 import 'package:world_clock/pages/home.dart';
+import 'package:world_clock/pages/loading.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    initialRoute: '/home',
+    routes: {
+      '/' : (context) => Loading(),
+      '/home' : (context) => Home(),
+      '/location' : (context) => ChooseLocation()
+    },
   ));
 }

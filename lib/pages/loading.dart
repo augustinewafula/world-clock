bdd86fb4a33a65faf5d5f0dbc7d014430cdf595f
augustinewafula <augustinetreezy@outlook.com>
2020-08-14 12:54:03 +0300

    add error handling to WorldTime class

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:world_clock/services/world_time.dart';
 
 class Loading extends StatefulWidget {
@@ -7,11 +8,14 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  String time = "Loading...";
 
   void setupWorldTime() async {
     WorldTime worldTime = WorldTime(location: "Nairobi", flag: "ke.png", url: "Africa/Nairobi");
     await worldTime.getTime();
-    print(worldTime.time);
+    setState(() {
+      time = worldTime.time;
+    });
   }
 
   @override
@@ -23,7 +27,11 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Loading"),
+      body: Padding(
+        padding: EdgeInsets.all(50),
+        child: Text(time),
+
+      )
     );
   }
 }
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -10,21 +10,26 @@ class WorldTime{
   WorldTime({ this.location, this.flag, this.url });
 
   Future<void> getTime() async {
-    var response = await http.get("http://worldtimeapi.org/api/timezone/$url");
-    Map data = convert.jsonDecode(response.body);
-    //print(data);
+    try {
+      var response = await http.get("http://worldtimeapi.org/api/timezone/$url");
+      Map data = convert.jsonDecode(response.body);
+      //print(data);
 
-    //get properties from data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(0, 3);
+      //get properties from data
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(0, 3);
 //    print(datetime);
 //    print(offset);
 
-    //create Datetime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      //create Datetime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
 
-    //set the time property
-    time = now.toString();
+      //set the time property
+      time = now.toString();
+    } catch (e) {
+      time = "Could not load time";
+    }
+
   }
 }

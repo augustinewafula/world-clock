ec36402fe8acafcac118102df495f3db1e430ad5
augustinewafula <augustinetreezy@outlook.com>
2020-08-14 16:41:44 +0300

    display location and time on home screen

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -17,16 +17,39 @@ class _HomeState extends State<Home> {
 
     return Scaffold(
       body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              FlatButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, "/location");
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text("Edit Location"),
-              )
-            ],
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+            child: Column(
+              children: <Widget>[
+                FlatButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/location");
+                  },
+                  icon: Icon(Icons.edit_location),
+                  label: Text("Edit Location"),
+                ),
+                SizedBox(height: 20,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                        data['location'],
+                        style: TextStyle(
+                          fontSize: 28,
+                          letterSpacing: 2
+                        ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 20,),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                      fontSize: 66
+                  ),
+                )
+              ],
+            ),
           )
       ),
     );
